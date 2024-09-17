b4f07f92af2548a99c1e54b7d1efb22b5b5658a5
김재우 <rlawodn6548@naver.com>
2024-09-17 17:47:39 +0900

    SpringBoot Application을 war로 빌드하는 코드 추가

---
+++ cicd/src/main/java/ServletInitializer.java
@@ -0,0 +1,11 @@
+import com.test.cicd.CicdApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CicdApplication.class);
+    }
+}
